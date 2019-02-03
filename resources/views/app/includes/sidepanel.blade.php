7d3c0743377fd073dca5badda4a49d41137030cb
bprayush <bprayush@yahoo.com>
2019-02-03 19:37:05 +0545

    implementing new designs --checking the sidepanel bug

--- resources/views/app/includes/sidepanel.blade.php
+++ resources/views/app/includes/sidepanel.blade.php
@@ -1,19 +1,3 @@
-{{-- <div class="col-md-4">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			Welcome, {{ $user->name }}
-		</div>
-		<div class="panel-body">
-			<ul class="list-group">
-				<li class="list-group-item"><a href="{{ route('app.index') }}">Dashboard</a></li>
-				<li class="list-group-item"><a href="{{ route('appreciation') }}">Appreciate Someone</a></li>
-				<li class="list-group-item">Appreciation History</li>
-				<li class="list-group-item">My Profile</li>
-			</ul>
-		</div>
-	</div>
-</div> --}}
-
 <nav id="sidebar">
 	<div class="sidebar-header">
 		<h3 class="logo">
