bd9243c738e5bc592fcc0c1d0accfdeb36a66d72
Aakash Raj Dahal <akashrajdahal@gmail.com>
2019-02-09 15:23:20 +0545

    Added the frontend for Appreciaton Page

--- resources/views/app/new_appreciate.blade.php
+++ resources/views/app/new_appreciate.blade.php
@@ -6,6 +6,19 @@ Momonation | Appreciate
 
 @section('content')
 
-hello
+<h3 class="text-info">
+	<i class="fas fa-thumbs-up"></i>&nbsp;
+	Appreciate
+</h3>
+
+<div style="margin-top:40px;">
+	<input type="text" class="form-control boxee" placeholder="Username or email"><br>
+	<input type="number" class="form-control boxee" placeholder="Amount of momo"><br>
+	<textarea name="" id="" cols="30" rows="4" class="form-control boxeetext" placeholder="Enter Message"></textarea><br>
+	<button class="btn btn-info blueboi">
+		<i class="fas fa-thumbs-up"></i>&nbsp;
+		Send Appreciation
+	</button>
+</div>
 
 @endsection
